ba712070b4e1222e35db3eb54de2f7edac09d6ad
JisooJang <jsjang0104@naver.com>
2025-12-19 20:01:51 +0900

    그만할래

--- backend/members/urls.py
+++ backend/members/urls.py
@@ -2,6 +2,15 @@
 
 from django.urls import path
 from .views import MemberViewSet # RegistrationView는 이제 사용 안 함
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+def create_admin(request):
+    if not User.objects.filter(username='admin').exists():
+        # 아이디: admin, 이메일: 없음, 비밀번호: 1234로 생성
+        User.objects.create_superuser('admin', '', '1234')
+        return HttpResponse("계정 생성 성공! 아이디: admin / 비번: 1234")
+    return HttpResponse("이미 계정이 있습니다.")
 
 urlpatterns = [
     # 4. (핵심) 'members/' -> '' (빈 문자열)로 변경
