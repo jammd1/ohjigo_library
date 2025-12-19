10b20d7946bd073fa9a73e2b5a329a78b62fe38e
JisooJang <jsjang0104@naver.com>
2025-12-19 20:08:35 +0900

    제발 돼라

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -7,6 +7,13 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from members.views import CustomTokenObtainPairView
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+def create_admin(request):
+    User.objects.create_superuser('admin', '', '1234') if not User.objects.filter(username='admin').exists() else None
+    return HttpResponse("Admin OK")
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("library/", include("library.urls")),
